4f33d7fd8874008b56a12a0e2f442f3ee785e311
karita <hayue0228@icloud.com>
2025-04-17 13:05:44 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+import { Client, GatewayIntentBits } from 'discord.js'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+})
+
+client.once('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`)
+})
+
+client.on('messageCreate', (message) => {
+  // ボット自身のメッセージは無視
+  if (message.author.bot) return
+
+  // メンションされたら反応
+  if (message.mentions.has(client.user)) {
+    message.reply('ちょ待てよ')
+  }
+})
+
+client.login(process.env.DISCORD_TOKEN)
